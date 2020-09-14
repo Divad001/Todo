67600e9f846aac41668e313cb9b0cd3ac864bff4
Divad001 <gyoredavid1993@gmail.com>
2020-09-14 15:47:28 +0200

    project and git init

---
+++ src/main/java/com/projects/todo/controllers/TodoController.java
@@ -0,0 +1,13 @@
+package com.projects.todo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TodoController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
